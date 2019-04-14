ab707575d921460efc116f1d293682a7dfaf0de6
Piotr Radziszewski <radnet.makis@o2.pl>
2019-04-14 21:55:31 +0200

    fix some issues

--- src/main/java/io/github/radd/mgrtestserver/config/WebSocketConfig.java
+++ src/main/java/io/github/radd/mgrtestserver/config/WebSocketConfig.java
@@ -1,15 +1,26 @@
 package io.github.radd.mgrtestserver.config;
 
 import io.github.radd.mgrtestserver.util.WebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new WebSocketHandler(), "/ws-connect");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(1000000);
+        //container.setMaxBinaryMessageBufferSize(8192);
+        return container;
     }
 }
--- src/main/java/io/github/radd/mgrtestserver/controller/HTTPController.java
+++ src/main/java/io/github/radd/mgrtestserver/controller/HTTPController.java
@@ -1,5 +1,7 @@
 package io.github.radd.mgrtestserver.controller;
 
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HTTPController {
 
     @PostMapping("/send")
-    public String sendMsg(@RequestBody String payload) {
+    public ResponseEntity sendMsg(@RequestBody String payload) {
         //System.out.println(payload);
-        return payload; // return received message
+        //return payload; // return received message
+
+        return ResponseEntity
+                .ok()
+                .cacheControl(CacheControl.noCache())
+                .body(payload);
+
     }
 
 }
